9d18aaed7c436f8efa0cf356e9aa41affb426cd1
Sinan Karabulut <142935021+karabulutsinan@users.noreply.github.com>
2024-03-20 00:41:10 +0300

    Update WebConfig.java

--- src/main/java/dev/patika/config/WebConfig.java
+++ src/main/java/dev/patika/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://vet-app-sinan-s-projects-a7c95102.vercel.app/","https://vetclinicrestapi-diiw.onrender.com")
+                .allowedOrigins("https://vet-app-sinan-s-projects-a7c95102.vercel.app","https://vetclinicrestapi-diiw.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
